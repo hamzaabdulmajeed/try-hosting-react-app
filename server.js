c421a35ac3bb20779764352c08d6b94467addc8d
muhammad hamza <hamzaabdulmajeed863@gmail.com>
2023-12-10 21:04:08 +0500

    crud-13

--- server.js
+++ server.js
@@ -1,44 +1,62 @@
-const express = require('express');
-const cors = require("cors");
-const path =require('path');
-const _dirname = path.resolve();
+// const express = require('express');
+// const cors = require("cors");
+// const path =require('path');
+// const _dirname = path.resolve();
 
-// const authRouter =require("./routes/auth.js");
-// const postRouter =require("./routes/post.js");
-// import commentRouter from './routes/comment.mjs'
-// import feedRouter from './routes/feed.mjs'
-// import postRouter from "./routes/post.mjs";
+// // const authRouter =require("./routes/auth.js");
+// // const postRouter =require("./routes/post.js");
+// // import commentRouter from './routes/comment.mjs'
+// // import feedRouter from './routes/feed.mjs'
+// // import postRouter from "./routes/post.mjs";
 
-const app = express();
-app.use(express.json()); // body parser
-// app.use(cors());
+// const app = express();
+// app.use(express.json()); // body parser
+// // app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Welcome to my server!");
-});
+// app.get("/", (req, res) => {
+//   res.send("Welcome to my server!");
+// });
 
-// /api/v1/login
-// app.use("/api/v1", authRouter);
+// // /api/v1/login
+// // app.use("/api/v1", authRouter);
 
-app.use((req, res, next) => {
-  // JWT
-  let token = "valid";
-  if (token === "valid") {
-    next();
-  } else {
-    res.status(401).send({ message: "invalid token" });
-  }
-});
+// app.use((req, res, next) => {
+//   // JWT
+//   let token = "valid";
+//   if (token === "valid") {
+//     next();
+//   } else {
+//     res.status(401).send({ message: "invalid token" });
+//   }
+// });
 
-// app.use("/api/v1", postRouter); // Secure api
+// // app.use("/api/v1", postRouter); // Secure api
 
-//     /static/vscode_windows.exe
-// app.use("/static", express.static(path.join(__dirname, "static")));
+// //     /static/vscode_windows.exe
+// // app.use("/static", express.static(path.join(__dirname, "static")));
 
-// app.use(express.static(path.join(__dirname, "static")));
+// // app.use(express.static(path.join(__dirname, "static")));
 
-const PORT = 4000;
+// const PORT = 4000;
+// app.listen(PORT, () => {
+//   console.log(`Example server listening on port ${PORT}`);
+// });
+const express = require('express')
+
+const app = express()
+const PORT = 4000
+
 app.listen(PORT, () => {
-  console.log(`Example server listening on port ${PORT}`);
-});
+  console.log(`API listening on PORT ${PORT} `)
+})
 
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 🥳')
+})
+
+app.get('/about', (req, res) => {
+  res.send('This is my about route..... ')
+})
+
+// Export the Express API
+module.exports = app
